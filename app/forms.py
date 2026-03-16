05165621839a782232a4a84f9716a06825c428f4
Tim060 <999666333111000@rambler.ru>
2026-03-17 00:43:14 +0300

    V5.0

--- app/forms.py
+++ app/forms.py
@@ -16,3 +16,20 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+    
+class MainForm(forms.Form):
+    name = forms.CharField(label='Ваше имя:', min_length=2, max_length=30)
+    country = forms.CharField(label='Ваша страна:', min_length=2, max_length=30)
+    internet = forms.ChoiceField(label = 'Как часто вы открываете наш ресурс?', 
+                               choices = (('1', 'Один раз в день'),
+                                ('2', 'Несколько раз в день'),
+                                ('3', 'Один раз в неделю'),
+                                ('4', 'Несколько раз в неделю'),
+                                ('5', 'Раз в месяц и реже')), initial=1) 
+    payment = forms.BooleanField(label='Вы используете платный контент?', required=False)
+    education = forms.ChoiceField(label = 'На каком этапе обучения вы находитесь?', 
+                               choices = (('1', 'Школа'),
+                                ('2', 'Колледж (техникум)'),
+                                ('3', 'Высшее учебное заведение'),
+                                ('4', 'Занимаюсь саомобучением')), widget=forms.RadioSelect, initial=1)
+    message = forms.CharField(label= 'Пожелания нашей платформе:', required=False, widget=forms.Textarea(attrs={'rows':5,'cols':50}))
--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Definition of views.
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from .forms import MainForm 
 
 def home(request):
     """Renders the home page."""
@@ -44,4 +45,43 @@ def about(request):
         }
     )
 def links(request):
-    return render(request, 'app/links.html', {})
+    return render(
+        request, 'app/links.html', 
+        {
+            'title':'Полезные ссылки',
+        }
+    )
+
+def pool(request):
+    assert isinstance(request, HttpRequest)
+    data = None
+    internet = {'1': 'Один раз в день', '2': 'Несколько раз в день',
+                '3': 'Один раз в неделю','4': 'Несколько раз в неделю',
+                '5': 'Раз в месяц и реже'}
+    education = {'1': 'Школа','2': 'Колледж (техникум)',
+                    '3': 'Высшее учебное заведение',
+                    '4': 'Занимаюсь саомобучением'}
+    if request.method == 'POST':
+        form = MainForm(request.POST)
+        if form.is_valid():
+            data = dict()
+            data['name'] = form.cleaned_data['name']
+            data['country'] = form.cleaned_data['country']
+            data['internet'] = internet [ form.cleaned_data['internet']]
+            if(form.cleaned_data['payment'] == True):
+                data['payment'] = 'Да'
+            else:
+                data['payment'] = 'Нет'
+            data['education'] =  education [form.cleaned_data ['education']]
+            data['message'] = form.cleaned_data['message']
+            form = None
+    else:
+        form = MainForm()
+    return render(
+        request, 'app/pool.html', 
+        {
+            'form': form,
+            'data': data,
+            'title':'Обратная связь',
+        }
+    )
